aa65c368fa62cfd3b2e8929ce10028af4ed406df
giantmolecules <giantmolecules@gmail.com>
2018-06-02 16:05:14 +0000

    arbitration

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from time import sleep
 from forms import BatLoForm, BatHiForm, CmdForm
 import time
 import smbus
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.IN)
+GPIO.setup(19, GPIO.OUT)
 
 bus = smbus.SMBus(3)
 
@@ -19,23 +24,40 @@ def index():
     if batloform.validate_on_submit():
         print(batloform.batloval.data)
         cmd1 = '{"batlo":"'+str(batloform.batloval.data)+'"}'
+        while GPIO.input(26):
+            print("Waiting...")
+        GPIO.output(19, GPIO.HIGH)
         for c in cmd1:
             bus.write_byte(0x77, ord(c))
             sleep(.01)
+        GPIO.output(19, GPIO.LOW)
         return redirect('/')
     if bathiform.validate_on_submit():
         print(bathiform.bathival.data)
         cmd2 = '{"bathi":"'+str(bathiform.bathival.data)+'"}'
+        while GPIO.input(26):
+            print("Waiting...")
+        GPIO.output(19, GPIO.HIGH)
         for c in cmd2:
             bus.write_byte(0x77, ord(c))
             sleep(.01)
+        GPIO.output(19, GPIO.LOW)
         return redirect('/')
     if cmdform.validate_on_submit():
         print(cmdform.cmdval.data)
         cmd3 = '{"cmd":"'+str(cmdform.cmdval.data)+'"}'
-        for c in cmd3:
-            bus.write_byte(0x77, ord(c))
-            sleep(.01)
+        #for c in cmd3:
+        #    bus.write_byte(0x77, ord(c))
+        #    sleep(.01)
+        while GPIO.input(26):
+            print("Waiting for I2C Bus")
+        GPIO.output(19, GPIO.HIGH)
+        try:
+            answer = bus.read_i2c_block_data(0x77, 0, 10)
+        except:
+            print("OOPS!")
+        print(answer)
+        GPIO.output(19, GPIO.LOW)
         return redirect('/')
     return render_template('index.html', title='form', batloform=batloform, bathiform=bathiform, cmdform=cmdform)
 
